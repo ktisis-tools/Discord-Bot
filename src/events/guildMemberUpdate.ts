db79d3b59fa76f3bd2ce0241d513cfd9efab697f
Junior <sudojunior@outlook.com>
2023-03-23 17:36:43 +0000

    clone roles array, rather than destructuring

--- src/events/guildMemberUpdate.ts
+++ src/events/guildMemberUpdate.ts
@@ -4,7 +4,7 @@ import { roleToAdd, roleTriggers } from "../util/config";
 export default (client: Client, guild: Guild, member: Member) => {
   if (guild.id !== process.env.DISCORD_GUILD_ID) return;
 
-  const { roles } = member;
+  const roles = member.roles.slice();
 
   let action: 'added' | 'removed' | null = null;
 
