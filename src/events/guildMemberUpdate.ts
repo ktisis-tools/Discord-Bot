4dc0bdbb1e071e0c837017cac107cbb433fa4312
Junior <sudojunior@outlook.com>
2023-03-23 18:42:35 +0000

    Add role grant logic to remove roles after trigger role is removed. * `DISCORD_GRANTED_ROLES` follows the same principle to `*_TRIGGER_ROLES` * reworked to use a slice of the member roles array which then gets compared by length before being pushed * action check was moved inside the scope as it is impossible to add or remove without a difference

--- src/events/guildMemberUpdate.ts
+++ src/events/guildMemberUpdate.ts
@@ -1,26 +1,43 @@
 import { Client, Guild, Member } from "eris";
-import { roleToAdd, roleTriggers } from "../util/config";
+import { grantedRoles, roleToAdd, roleTriggers } from "../util/config";
 
-export default (client: Client, guild: Guild, member: Member) => {
+export default async (client: Client, guild: Guild, member: Member) => {
   if (guild.id !== process.env.DISCORD_GUILD_ID) return;
 
   const roles = member.roles.slice();
+  const roleIndex = roles.indexOf(roleToAdd);
 
   let action: 'added' | 'removed' | null = null;
 
   if (roles.some(r => roleTriggers.includes(r))) {
-    if (!roles.includes(roleToAdd)) {
-      member.addRole(roleToAdd);
+    if (roleIndex < 0) {
+      roles.push(roleToAdd);
       action = 'added';
     }
   } else {
     if (roles.includes(roleToAdd)) {
-      member.removeRole(roleToAdd);
+      roles.splice(roleIndex, 1);
       action = 'removed';
+    }
+
+    /**
+     * Remove all roles previously granted by other means, regardless of action.
+     * Action does not change if any roles are removed, as it was not a trigger or target role of primary focus.
+     */
+    for (const grantedRole of grantedRoles) {
+      const targetIndex = roles.indexOf(grantedRole);
+
+      if (targetIndex >= 0) {
+        roles.splice(targetIndex, 1);
+      }
     }
   }
 
-  if (action) {
-    console.log(`[${new Date().toUTCString()}] Role ${roleToAdd} was ${action} for ${member.username}.`);
+  if (roles.length !== member.roles.length) {
+    await member.edit({ roles });
+
+    if (action) {
+      console.log(`[${new Date().toUTCString()}] Role ${roleToAdd} was ${action} for ${member.username}.`);
+    }
   }
 }
