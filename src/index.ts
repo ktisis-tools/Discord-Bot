78fd125778ce6a7136e68ae8bdd8677e80422cc5
Junior <sudojunior@outlook.com>
2023-06-13 22:10:57 +0100

    attach member add event to client

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ let dotenvPath = path.join(process.cwd(), '.env');
 if (path.parse(process.cwd()).name === 'dist') dotenvPath = path.join(process.cwd(), '..', '.env');
 dotenv.config({ path: dotenvPath });
 
+import guildMemberAdd from './events/guildMemberAdd.js';
 import guildMemberUpdateEvent from './events/guildMemberUpdate.js';
 import readyEvent from './events/ready.js';
 
@@ -15,6 +16,7 @@ const client = new Client(process.env.DISCORD_BOT_TOKEN, {
   autoreconnect: true
 });
 
+client.on('guildMemberAdd', guildMemberAdd.bind(null, client));
 client.on('guildMemberUpdate', guildMemberUpdateEvent.bind(null, client));
 client.on('ready', readyEvent.bind(null, client));
 
