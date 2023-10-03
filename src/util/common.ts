a1d0d766a1b8a4e312a77046e0e18133b91f54d7
Junior <sudojunior@outlook.com>
2023-10-03 21:32:46 +0100

    refactor fixed interval to class

--- src/util/common.ts
+++ src/util/common.ts
@@ -21,10 +21,37 @@ export const hasSomeTrigger = (roles: string[]) => roles.some(role => roleTrigge
  * Run the {@link callback} at each {@link interval}, with options to {@link offset} and {@link shortCall}.
  */
 export function setFixedInterval(interval: number, offset: number, shortCall: boolean, callback: Function) {
-  setTimeout(() => {
-    if (shortCall) callback();
-    setInterval(() => {
-      callback();
-    }, interval);
-  }, interval - Date.now() % interval + offset);
+  return new FixedInterval(interval, offset, shortCall, callback);
+}
+
+class FixedInterval {
+  isFirstCall: boolean = true;
+  ref: NodeJS.Timer;
+
+  constructor(
+    private interval: number,
+    private offset: number,
+    private shortCall: boolean,
+    private callback: Function
+  ) {
+    this.run();
+  }
+
+  get timeUntilNextCall(): number {
+    return this.interval - Date.now() % this.interval + this.offset;
+  }
+
+  private run() {
+    this.ref = setTimeout(() => {
+      if (!this.isFirstCall || this.shortCall) {
+        this.callback();
+      }
+      this.isFirstCall = false;
+      this.run();
+    }, this.timeUntilNextCall);
+  }
+
+  destroy() {
+    if (this.ref) clearTimeout(this.ref);
+  }
 }
